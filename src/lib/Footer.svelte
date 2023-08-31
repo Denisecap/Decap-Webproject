54a7c4c1a3bcb49a03afabc04ff5525d79999285
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-09-01 11:49:53 +1200

    Work done

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -4,5 +4,24 @@
 
 
 
-<p>YOUR DREAM SHOES</p>
 
+<p>Shop By Sports</p>
+
+
+
+
+
+<div class="gallery">
+    <div>
+        <div class="image" style="background-image: url(/img/Basketball.jpg);"></div>
+        <div>Basketball</div>
+    </div>
+    <div>
+        <div class="image" style="background-image: url(/img/Football.jpg);"></div>
+        <div>Football</div>
+    </div>
+    <div>
+        <div class="image" style="background-image: url(/img/Running.jpg)"></div> 
+        <div>Running</div>
+    </div>                                                                               
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,27 @@
     JUST DO IT
 </footer>
 
+
+
 <h1>
     SHOP NOW
 </h1>
 
+<h1>
+    YOUR DREAM SHOES
+</h1>
 
 
 
 <div class="gallery">
     <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/max_632/884cd2102018173.5f2c7c7e72091.jpg);"></div>
     <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/projects/max_808/a58984102018173.Y3JvcCw0MjI5LDMzMDgsNTMzLDE3Nw.jpg);"></div>
-    <div>   
-        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/884cd2102018173.5f2c7c7e72091.jpg" alt="shoe" >                                                                              
-    </div>
-
+    <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/disp/884cd2102018173.5f2c7c7e72091.jpg)"></div>                                                                                  
 </div>
 
 
 
-    
+
+
+
 
