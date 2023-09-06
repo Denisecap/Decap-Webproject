d6ecbf13c72cf7b616b6262407bb7e80bab3309f
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-09-07 09:54:42 +1200

    added some work

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -14,11 +14,17 @@
 <h1>
     YOUR DREAM SHOES
 </h1>
-<div class="gallery gallery2">
-    <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/max_632/884cd2102018173.5f2c7c7e72091.jpg);"></div>
-    
-    <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/projects/max_808/a58984102018173.Y3JvcCw0MjI5LDMzMDgsNTMzLDE3Nw.jpg);"></div>
-    <div style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/disp/884cd2102018173.5f2c7c7e72091.jpg)"></div>                                                                                  
+<div class="photos">
+    <img src="/img/shoes.jpg" alt="">
+
+     <img src="/img/shoe2.jpg" alt="">
+
+    <img src="/img/shoes3.jpg" alt="">
 </div>
+<div class="">
+</div>
+
+    
+
 
 
