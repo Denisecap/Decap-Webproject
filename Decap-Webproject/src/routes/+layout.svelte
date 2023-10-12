6ac94d5e25be6303bc86dc213189a97d9e465a1d
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-13 12:36:29 +1300

    main commit

---
+++ Decap-Webproject/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+
+<script>
+    
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="css/main.css" />
+</svelte:head>
+
+
+
+<slot/>
+
+<Footer/>
+    
+
