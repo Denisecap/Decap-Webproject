68565dfb5b7fcbfe91be6e464956045320bc7d52
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-24 12:28:17 +1300

    Fixed the footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,25 +1,39 @@
 <script>
     import Nav from '$lib/Nav.svelte';
 </script>
-
-<style>
-    .footer {
-      position: relative;
-      left: 0;
-      bottom: 50%;
-      width: 100%;
-      height: 100%;
-      size: 100%;
-      background-color:black;
-      color: white;
-      text-align: left;
-
-    }
-    </style>
-    
+ 
     <div class="footer">
       <p>Nike</p>
       <h1>
        
       </h1>
     </div>
+
+    <footer>
+      <div class="footer-container">
+          <div class="footer-logo">
+              <img src="nike-logo.png" alt="Nike Logo">
+          </div>
+          <div class="footer-links">
+              <ul>
+                  <li><a href="#">Shop</a></li>
+                  <li><a href="#">Collections</a></li>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+          <div class="footer-social">
+              <ul>
+                  <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+          </div>
+      </div>
+      <div class="footer-disclaimer">
+          &copy; 2023 Nike Store. All Rights Reserved.
+      </div>
+  </footer>
+  
+  
+
