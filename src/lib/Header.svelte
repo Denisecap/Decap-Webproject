ef4c93681f92288035f73f5a7562aa6e74f1d5ea
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-26 11:33:11 +1300

    removed the styles from header and nav since its working after I tested it and put it in main.css

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,10 +2,6 @@
     import Nav from '$lib/Nav.svelte';
 </script>
 
-
-
-
-
 <div class="background">
     <header>
         <img src="https://logowik.com/content/uploads/images/697_nike.jpg" width= "200"height="200">
@@ -13,13 +9,3 @@
     </header>
 </div>
  
- 
-
-<style>
-    header
-    {
-        background-color: white(0, 174, 255);
-        color: black;
-        font-size: large;       
-    }         
-</style>
