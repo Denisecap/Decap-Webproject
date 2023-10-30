888ae56ddd6133718c513c209a8ba87f9366b8bb
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-31 12:53:12 +1300

    added content for Shop

--- src/routes/Shop/+page.svelte
+++ src/routes/Shop/+page.svelte
@@ -1,3 +1,46 @@
-<h1>
-    fdfsdfsdf
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nike Shop</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   
+
+    <main>
+        <h1>Welcome to Nike Shop</h1>
+
+        <div class="shop-info">
+            <p>Discover the latest Nike products for sports, fitness, and lifestyle. We offer a wide range of Nike shoes, apparel, and accessories for men, women, and kids. Explore our collections and find the perfect gear to support your active lifestyle.</p>
+        </div>
+
+        <div class="featured-products">
+            <h2>Featured Products</h2>
+            <div class="product">
+                <img src="product1.jpg" alt="Nike Product 1">
+                <h3>Nike Running Shoes</h3>
+                <p>Experience comfort and performance with these top-rated running shoes.</p>
+                <a href="#">Shop Now</a>
+            </div>
+            <div class="product">
+                <img src="product2.jpg" alt="Nike Product 2">
+                <h3>Nike Sportswear</h3>
+                <p>Elevate your style with the latest Nike sportswear collection.</p>
+                <a href="#">Shop Now</a>
+            </div>
+            <!-- Add more featured products here -->
+        </div>
+    </main>
+
+    <footer>
+        <div class="social-media">
+            <a href="#"><img src="facebook.png" alt="Facebook"></a>
+            <a href="#"><img src="twitter.png" alt="Twitter"></a>
+            <a href="#"><img src="instagram.png" alt="Instagram"></a>
+        </div>
+        <p>&copy; 2023 Nike Shop. All rights reserved.</p>
+    </footer>
+</body>
+</html>
