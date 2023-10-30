81e749a631c211e75bd07d807b24c9e273bfc240
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-31 12:39:24 +1300

    added page content for Contacts

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   
+
+    <main>
+        <h1>Contact Us</h1>
+
+        <div class="contact-info">
+            <div class="contact-item">
+                <h2>Email</h2>
+                <p>caponpondenise6@gmail.com</p>
+            </div>
+            <div class="contact-item">
+                <h2>Phone</h2>
+                <p>(021) 269 5740</p>
+            </div>
+            <div class="contact-item">
+                <h2>Address</h2>
+                <p>597, George Street</p>
+            </div>
+        </div>
+
+        <div class="contact-form">
+            <h2>Send Us a Message</h2>
+            <form>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="message">Message</label>
+                <textarea id="message" name="message" rows="4" required></textarea
 
     
 
