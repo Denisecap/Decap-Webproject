e56ee51fbbf4b034e9ae5fcb4caac15c66532bb0
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-31 12:47:57 +1300

    added content in my Collections

---
+++ src/routes/Collections/+page.svelte
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Collections</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <!-- Your navigation menu, logo, and other header content -->
+    </header>
+
+    <main>
+        <h1>Nike Collections</h1>
+
+        <div class="collection">
+            <h2>Summer Collection</h2>
+            <p>Explore our latest summer collection, perfect for the hot season and outdoor activities.</p>
+            <a href="summer-collection.html">View Collection</a>
+        </div>
+
+        <div class="collection">
+            <h2>Fall Collection</h2>
+            <p>Stay comfortable and stylish with our autumn-inspired fall collection.</p>
+            <a href="fall-collection.html">View Collection</a>
+        </div>
+
+        <div class="collection">
+            <h2>Holiday Collection</h2>
+            <p>Celebrate the holidays in style with our festive holiday collection.</p>
+            <a href="holiday-collection.html">View Collection</a>
+        </div>
+    </main>
+
+    <footer>
+        <!-- Your footer content and links to social media -->
+    </footer>
+</body>
+</html>
