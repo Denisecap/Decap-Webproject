2ef2b14ac83749dbfc0ccdb84ea69cb715fe8653
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-10-31 12:43:35 +1300

    added content for About us

---
+++ src/routes/About Us/+page.svelte
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Nike Shop</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <!-- Your navigation menu, logo, and other header content -->
+    </header>
+
+    <main>
+        <h1>About Nike Shop</h1>
+
+        <div class="about-text">
+            <p>Welcome to Nike Shop, your one-stop destination for all things Nike. We are passionate about sports, fitness, and style, and we're here to provide you with the latest and greatest Nike products. Whether you're an athlete, a fitness enthusiast, or simply love the Nike brand, you'll find everything you need right here.</p>
+        </div>
+
+        <div class="mission">
+            <h2>Our Mission</h2>
+            <p>At Nike Shop, our mission is to inspire and equip individuals to be the best version of themselves through the power of sports and athleticism. We believe that everyone has the potential to achieve greatness, and we're committed to helping you reach your goals.</p>
+        </div>
+    </main>
+
+    <footer>
+        <!-- Your footer content and links to social media -->
+    </footer>
+</body>
+</html>
