fc4404e800ab546cb4017291b023985628cee0c1
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-02 13:16:19 +1300

    added men's page

---
+++ src/routes/Men/+page.svelte
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Men's Nike Collection</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <!-- Your navigation menu, logo, and other header content -->
+    </header>
+
+    <main>
+        <h1>Men's Nike Collection</h1>
+
+        <div class="collection-info">
+            <p>Discover our extensive collection of Nike products tailored for men. Whether you're into sports or seeking casual and stylish attire, we have a wide range of Nike gear to complement your lifestyle and meet your performance needs.</p>
+        </div>
+
+        <div class="product-list">
+            <div class="product">
+                <img src="product1.jpg" alt="Product 1">
+                <h3>Nike Running Shoes</h3>
+                <p>Experience comfort and performance with these top-rated running shoes.</p>
+            </div>
+            <div class="product">
+                <img src="product2.jpg" alt="Product 2">
+                <h3>Nike Basketball Jersey</h3>
+                <p>Elevate your game with our high-quality basketball jerseys.</p>
+            </div>
+            <!-- Add more product items here -->
+        </div>
+    </main>
+
+    <footer>
+        <!-- Your footer content and links to social media -->
+    </footer>
+</body>
+</html>
