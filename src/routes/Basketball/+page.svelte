a9a7d996fefdfd581a93b5d68bfcd1da46a8424e
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 14:48:44 +1300

    deleted one in basketball'

--- src/routes/Basketball/+page.svelte
+++ src/routes/Basketball/+page.svelte
@@ -15,7 +15,6 @@
                     <li><a href="#features">Features</a></li>
                     <li><a href="#products">Products</a></li>
                     <li><a href="#reviews">Reviews</a></li>
-                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </nav>
