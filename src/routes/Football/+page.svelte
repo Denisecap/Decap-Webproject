114c1882a88e7f72095f90679fead43596490fa2
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 15:07:08 +1300

    added a new page for football

--- src/routes/Football/+page.svelte
+++ src/routes/Football/+page.svelte
@@ -1,2 +1,64 @@
 
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nike Football</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <h1>Nike Football</h1>
+                <ul>
+                    <li><a href="#about">About Nike Football</a></li>
+                    <li><a href="#products">Nike Football Products</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section id="about">
+        <div class="container">
+            <h2>About Nike Football</h2>
+            <p>Nike Football is dedicated to delivering top-quality football gear and products designed for performance on the field. With a focus on innovation and style, Nike football products help athletes achieve their best.</p>
+        </div>
+    </section>
+
+    <section id="products">
+        <div class="container">
+            <h2>Nike Football Products</h2>
+            <div class="product-card">
+                <img src="nike-football-boots1.jpg" alt="Nike Football Boots 1">
+                <h3>Nike Mercurial Superfly</h3>
+                <p>The Nike Mercurial Superfly is known for its speed and agility. It's a favorite among many football players for its sleek design and performance features.</p>
+                <p>Price: $199.99</p>
+            </div>
+            <div class="product-card">
+                <img src="nike-football-boots2.jpg" alt="Nike Football Boots 2">
+                <h3>Nike Tiempo Legend</h3>
+                <p>The Nike Tiempo Legend is known for its classic style and durability. It offers exceptional comfort and control on the pitch.</p>
+                <p>Price: $179.99</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <h2>Contact Us</h2>
+            <p>If you have questions about Nike football products or need assistance, please don't hesitate to contact us. We're here to help.</p>
+            <a class="btn" href="mailto:info@nikefootball.com">Email Us</a>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2023 Nike Football. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
+
 
