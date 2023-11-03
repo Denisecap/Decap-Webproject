c961f90b333e90ed0f7e2a2da90f3d944b0340f8
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 14:52:01 +1300

    added a page fot football'

--- src/routes/Football/+page.svelte
+++ src/routes/Football/+page.svelte
@@ -1,20 +1,65 @@
-<h1>
-    FOOTBALL/MEN
-</h1>
 
-<div class="photos">
-    <div class="photo1">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Football Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <h1>Football World</h1>
+                <ul>
+                    <li><a href="#matches">Matches</a></li>
+                    <li><a href="#teams">Teams</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
-    </div>
-    <div class="photo2">
+    <section id="matches">
+        <div class="container">
+            <h2>Upcoming Matches</h2>
+        
+        </div>
+    </section>
 
-    </div>
-</div>
-<div class="photos">
-    <div class="photo3">
+    <section id="teams">
+        <div class="container">
+            <h2>Top Football Teams</h2>
+           
+        </div>
+    </section>
 
-    </div>
-    <div class="photo4">
+    <section id="gallery">
+        <div class="container">
+            <h2>Football Gallery</h2>
+            <div class="image-gallery">
+                <img src="football-image1.jpg" alt="Football Image 1">
+                <img src="football-image2.jpg" alt="Football Image 2">
+                <img src="football-image3.jpg" alt="Football Image 3">
+                
+            </div>
+        </div>
+    </section>
 
-    </div>
-</div>
+    <section id="contact">
+        <div class="container">
+            <h2>Contact Us</h2>
+            <p>If you have any questions or want to get in touch, please feel free to contact us.</p>
+            <a class="btn" href="mailto:info@footballworld.com">Email Us</a>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2023 Football World. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
+
