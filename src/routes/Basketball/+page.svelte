d03ff1d61a87ca24b61453ef2eac40c694eccea9
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 14:45:25 +1300

    added content for basketball

--- src/routes/Basketball/+page.svelte
+++ src/routes/Basketball/+page.svelte
@@ -1,4 +1,59 @@
 
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basketball Women's Shoes | Your Brand Name</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <h1>Your Brand Name</h1>
+                <ul>
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#products">Products</a></li>
+                    <li><a href="#reviews">Reviews</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section id="hero">
+        <div class="container">
+            <h1>Basketball Women's Shoes</h1>
+            <p>Experience comfort, style, and top-notch performance on the court with our premium collection of basketball women's shoes.</p>
+            <a class="btn" href="#products">Explore Our Products</a>
+        </div>
+    </section>
+
+    <section id="products">
+        <div class="container">
+            <h2>Product Lineup</h2>
+            <div class="product-card">
+                <img src="nike-air-max.jpg" alt="Nike Air Max">
+                <h3>Nike Air Max</h3>
+                <p>The classic Nike Air Max sneakers with Air cushioning.</p>
+                <p>Price: $129.99</p>
+            </div>
+            <div class="product-card">
+                <img src="react-element-55.jpg" alt="React Element 55">
+                <h3>Nike React Element 55</h3>
+                <p>Comfortable and stylish Nike React Element shoes.</p>
+                <p>Price: $99.99</p>
+            </div>
+         
+        </div>
+    </section>
+
+    
+  
+</body>
+</html>
+
+
 
 
 
