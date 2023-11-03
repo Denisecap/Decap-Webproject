ddeb8fd561770290112042dd8780c4652dab4676
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 14:59:12 +1300

    added a page for running

--- src/routes/Running/+page.svelte
+++ src/routes/Running/+page.svelte
@@ -1,20 +1,56 @@
-<h1>
-    RUNNING
-</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Running Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <h1>Running World</h1>
+                <ul>
+                    <li><a href="#about">About Running</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
-<div class="photos">
-    <div class="photo1">
+    <section id="about">
+        <div class="container">
+            <h2>About Running</h2>
+            <p>Running is a popular form of exercise that offers numerous health benefits. It is an excellent way to stay fit, improve cardiovascular health, and relieve stress. Whether you're a beginner or an experienced runner, there's a lot to discover in the world of running.</p>
+        </div>
+    </section>
 
-    </div>
-    <div class="photo2">
+    <section id="gallery">
+        <div class="container">
+            <h2>Running Gallery</h2>
+            <div class="image-gallery">
+                <img src="running-image1.jpg" alt="Running Image 1">
+                <img src="running-image2.jpg" alt="Running Image 2">
+                <img src="running-image3.jpg" alt="Running Image 3">
+         
+            </div>
+        </div>
+    </section>
 
-    </div>
-</div>
-<div class="photos">
-    <div class="photo3">
+    <section id="contact">
+        <div class="container">
+            <h2>Contact Us</h2>
+            <p>If you have any questions or want to share your running experiences, please feel free to contact us. We'd love to hear from you.</p>
+            <a class="btn" href="mailto:info@runningworld.com">Email Us</a>
+        </div>
+    </section>
 
-    </div>
-    <div class="photo4">
-
-    </div>
-</div>
+    <footer>
+        <div class="container">
+            <p>&copy; 2023 Running World. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
