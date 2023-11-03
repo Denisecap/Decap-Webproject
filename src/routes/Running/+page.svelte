ed0f6dd447355720896cfff496dc9d099788a56e
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-03 15:04:54 +1300

    added a different style of running page

--- src/routes/Running/+page.svelte
+++ src/routes/Running/+page.svelte
@@ -1,19 +1,20 @@
-<!DOCTYPE html>
+
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Running Page</title>
+    <title>Nike Running</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
     <header>
         <nav>
             <div class="container">
-                <h1>Running World</h1>
+                <h1>Nike Running</h1>
                 <ul>
-                    <li><a href="#about">About Running</a></li>
-                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#about">About Nike Running</a></li>
+                    <li><a href="#products">Nike Running Products</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </nav>
@@ -21,30 +22,38 @@
 
     <section id="about">
         <div class="container">
-            <h2>About Running</h2>
-            <p>Running is a popular form of exercise that offers numerous health benefits. It is an excellent way to stay fit, improve cardiovascular health, and relieve stress. Whether you're a beginner or an experienced runner, there's a lot to discover in the world of running.</p>
+            <h2>About Nike Running</h2>
+            <p>Nike Running is dedicated to helping runners reach their full potential. With a commitment to innovation and performance, Nike running products provide the support and comfort runners need to achieve their goals.</p>
         </div>
     </section>
 
-    <section id="gallery">
+    <section id="products">
         <div class="container">
-            <div class="image-gallery">
-                <img src="running-image1.jpg" alt="Running Image 1">
-                <img src="running-image2.jpg" alt="Running Image 2">
-                <img src="running-image3.jpg" alt="Running Image 3">
-         
+            <h2>Nike Running Products</h2>
+            <div class="product-card">
+                <img src="nike-running-shoe1.jpg" alt="Nike Running Shoe 1">
+                <h3>Nike Air Zoom Pegasus</h3>
+                <p>The Nike Air Zoom Pegasus is a versatile running shoe designed for comfort and speed. Perfect for daily runs and races.</p>
+                <p>Price: $129.99</p>
             </div>
+            <div class="product-card">
+                <img src="nike-running-shoe2.jpg" alt="Nike Running Shoe 2">
+                <h3>Nike Free RN</h3>
+                <p>The Nike Free RN offers a barefoot-like feel with cushioning, making it great for natural running movements.</p>
+                <p>Price: $99.99</p>
+            </div>
+      
         </div>
     </section>
 
     <section id="contact">
         <div class="container">
             <h2>Contact Us</h2>
-            <p>If you have any questions or want to share your running experiences, please feel free to contact us. We'd love to hear from you.</p>
-            <a class="btn" href="mailto:info@runningworld.com">Email Us</a>
+            <p>If you have questions about Nike running products or need assistance, please don't hesitate to contact us. We're here to help.</p>
+            <a class="btn" href="mailto:info@nikerunning.com">Email Us</a>
         </div>
     </section>
-
-
 </body>
 </html>
+
+
