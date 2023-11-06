ddac1b8e12a1973366c0305d87c65c65c6251f80
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-07 10:50:29 +1300

    Fixed code

--- src/routes/Basketball/+page.svelte
+++ src/routes/Basketball/+page.svelte
@@ -1,4 +1,3 @@
-<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -49,7 +48,7 @@
     
   
 </body>
-</html>
+
 
 
 
--- src/routes/Football/+page.svelte
+++ src/routes/Football/+page.svelte
@@ -1,4 +1,3 @@
-<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -52,6 +51,5 @@
         </div>
     </section>
 </body>
-</html>
 
 
