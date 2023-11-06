2b32a9759d18ed1cfc5f52e3f319cf896d65614b
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-07 10:51:58 +1300

    Fixed code for running

--- src/routes/Running/+page.svelte
+++ src/routes/Running/+page.svelte
@@ -1,6 +1,4 @@
-
-<html>
-<head>
+q<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nike Running</title>
@@ -54,6 +52,6 @@
         </div>
     </section>
 </body>
-</html>
+
 
 
