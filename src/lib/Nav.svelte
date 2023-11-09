33a439326937d0fc309c38d44f0a26ab49bea68e
Denisecap <131939828+Denisecap@users.noreply.github.com>
2023-11-09 13:09:58 +1300

    adding more designs

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,3 +4,15 @@
     <a href="../src/routes/Men/+page.svelte">Men</a>
  
 </nav>
+
+
+<nav class="sidebar-nav">
+    <a href="../src/routes/Women/+page.svelte">
+        <i class="fas fa-female"></i> Women
+    </a>
+    <a href="../src/routes/Men/+page.svelte">
+        <i class="fas fa-male"></i> Men
+    </a>
+</nav>
+
+
